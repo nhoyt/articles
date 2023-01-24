30e4b7a765cc49b33b091d59eecc5792ef5600c5
Nicholas Hoyt <nhoyt@illinois.edu>
2023-01-23 18:06:04 -0600

    Reorganize folders

--- nav-menus-01/build.sh
+++ nav-menus-01/build.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-for f in part-?.md ; do sed -e '$s/$/\n/' $f ; done > article.md
+for f in part-?.md ; do sed -e '$s/$/\n/' $f ; done > ../build/nav-menus-01.md
--- nav-menus-02/build.sh
+++ nav-menus-02/build.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-for f in part-?.md ; do sed -e '$s/$/\n/' $f ; done > article.md
+for f in part-?.md ; do sed -e '$s/$/\n/' $f ; done > ../build/nav-menus-02.md
